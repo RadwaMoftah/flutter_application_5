7cf55d185a35e8b2ddaa0ac576fd3259e01c104b
RadwaMoftah <rm1919@fayoum.edu.eg>
2023-03-27 02:09:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FirstApp(),
+    );
+  }
+}
+
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Application"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Center(
+        child: Text(
+          "Radwa Moftah Mahmoud"),
+     ),);}
+}
